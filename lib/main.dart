f4b334bc5c259181ea5221532e6ae90f943c398e
est_calexx <10johannesmunoz@gmail.com>
2025-09-11 21:18:23 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(
+    XylophoneApp()
+  );
+}
+
+class XylophoneApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioPlayer();
+                player.audioCache = AudioCache();
+                player.play(AssetSource('note1.wav'));
+              },
+              child:
+                Text('Click Me!'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
